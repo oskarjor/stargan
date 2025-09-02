8d23936e334868e82645f7804b68beaa848cef29
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-09-02 09:47:26 +0200

    update logger to use tensorboard

--- logger.py
+++ logger.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+from torch.utils.tensorboard import SummaryWriter
 
 
 class Logger(object):
@@ -6,9 +6,14 @@ class Logger(object):
 
     def __init__(self, log_dir):
         """Initialize summary writer."""
-        self.writer = tf.summary.FileWriter(log_dir)
+        self.writer = SummaryWriter(log_dir)
 
     def scalar_summary(self, tag, value, step):
         """Add scalar summary."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        self.writer.add_scalar(tag, value, step)
+
+
+if __name__ == "__main__":
+    logger = Logger(log_dir="logs")
+    logger.scalar_summary("test", 1.0, 1)
+    logger.writer.close()
