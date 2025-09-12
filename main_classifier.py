b126e987e03cddcb2dd5c770fec0dd283d46671c
Oskar Jorgensen <oskarjorgensen@gmail.com>
2025-09-12 09:44:26 +0200

    update nargs for attributes

--- main_classifier.py
+++ main_classifier.py
@@ -120,6 +120,7 @@ if __name__ == "__main__":
     parser.add_argument("--image_size", type=int, default=128)
     parser.add_argument(
         "--selected_attrs",
+        nargs="+",
         type=str,
         default=["Young"],
     )
